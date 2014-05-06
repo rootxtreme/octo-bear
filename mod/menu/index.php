37c8b8a8ccab2dae303a0c4fa2ac560b9ce8e487
Georoot <--global>
2014-05-06 18:05:09 +0530

    method for getting menu added

--- mod/menu/index.php
+++ mod/menu/index.php
@@ -1,7 +1,19 @@
 <?php
 class menu{
     public function __construct(){
-        
+        $this -> getMenu();
+        $this -> addMenu();
+        $this -> delete();
+    }
+    
+    public function getMenu(){
+        if($_POST['cmd'] == 'getMenu'){
+            include_once('../../core/db.php');   
+            $dbMnaager = new db();
+            $result = $dbMnaager -> execute('select * from data;');
+            $result = json_encode($result);
+            print $result;
+        }
     }
 
 
